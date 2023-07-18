7424e2b7cadb16824fcec0948c25dff2928163f6
ArnoldPaul17 <paularnold17@gmail.com>
2023-07-18 16:11:32 -0300

    initial proyect

---
+++ app/Componentes/Nav.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import './nav.css';
+
+export default function Nav () {
+    return (
+        <nav className='nav'>
+            <ul className='nav-lista'>
+                <li className='nav-item'>
+                    <Link href='./'>Inicio</Link>
+                </li>
+                <li className='nav-item'>
+                <Link href='./servicios'>Servicios</Link>
+                </li>
+                <li className='nav-item'>
+                <Link href='./contacto'>Contacto</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
---
+++ app/servicios/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Servicios () {
+    return (
+        <h1>Estas en servicios</h1>
+    )
+}
